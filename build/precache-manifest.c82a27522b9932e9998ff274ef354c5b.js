0d09a4091aa14b44135e19a928e7ae8803884417
pben2021 <49542873+pben2021@users.noreply.github.com>
2022-08-07 09:56:23 -0400

    w/ build and updated package-lock

---
+++ build/precache-manifest.c82a27522b9932e9998ff274ef354c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a970571507d4165c448",
+    "url": "/static/css/main.aa343f41.chunk.css"
+  },
+  {
+    "revision": "9a970571507d4165c448",
+    "url": "/static/js/main.cd0040b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd53471dfe303a66a6c2",
+    "url": "/static/js/2.487bc2d4.chunk.js"
+  },
+  {
+    "revision": "c1286b2371c0d95e62975dc809ae3266",
+    "url": "/static/media/instructions.c1286b23.png"
+  },
+  {
+    "revision": "e324eefd22e1140dda45ce7e35ed47a8",
+    "url": "/index.html"
+  }
+];
